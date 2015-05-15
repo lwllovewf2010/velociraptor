8a5d540aeba9d05c329f3790022e20e182a04927
M Hillman <mdhillman@cmclinnovations.com>
2015-05-15 15:51:41 +0100

    develop: fixed gradle issues with project dependancies by regenerating libGDX files.

---
+++ core/src/terrain/generation/Perlin.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2015 M Hillman - All Rights Reserved
+ * 
+ * This application and all inherent data, source files, information and graphics are 
+ * the copyright and sole property of M Hillman (thisishillman.co.uk).
+ * 
+ * Any unauthorised redistribution or reproduction of part, or all, of the contents of this 
+ * application in any form is prohibited under UK Copyright Law. You may not, except with the 
+ * express written permission of CMCL Innovations, distribute or commercially exploit this
+ * application or it's content. All other rights reserved.
+ * 
+ * For more information please contact business<@>thisishillman.co.uk
+ */
+package terrain.generation;
+
+public class Perlin {
+
+	
+	public double perlin(double x, double y) {
+		return 0;
+	}
+	
+}
+//End of class.
